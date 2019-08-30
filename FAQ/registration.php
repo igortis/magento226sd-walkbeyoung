00c1af26a7e89509635ddce84c6e7dd07e6a7cdf
igortis <44641113+igortis@users.noreply.github.com>
2019-08-30 21:24:48 +0300

    Add module FAQ

---
+++ FAQ/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 29.07.19
+ * Time: 12:43
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'DevLab_FAQ',
+    __DIR__
+);
