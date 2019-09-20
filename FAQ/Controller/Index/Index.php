59325230b9d5a95c7ebb1814e470e509e6706a3d
igortis <44641113+igortis@users.noreply.github.com>
2019-09-20 13:04:20 +0300

    Update Index.php
    Add setting to SEO custom module

--- FAQ/Controller/Index/Index.php
+++ FAQ/Controller/Index/Index.php
@@ -7,24 +7,51 @@
  */
 namespace DevLab\FAQ\Controller\Index;
 
-use \Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Page\Config;
+use Magento\Store\Model\ScopeInterface;
+
+
+
+
 
 class Index extends Action
 {
     /** @var  \Magento\Framework\View\Result\Page */
     protected $resultPageFactory;
+    protected $scopeConfig;
+    protected $pageConfig;
     /**
      * @param \Magento\Framework\App\Action\Context $context
      */
-    public function __construct(\Magento\Framework\App\Action\Context $context,
-                                \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    public function __construct(Context $context,
+                                PageFactory $resultPageFactory,
+                                ScopeConfigInterface $scopeConfig,
+                                Config $pageConfig)
     {
         $this->resultPageFactory = $resultPageFactory;
+        $this->scopeConfig = $scopeConfig;
+        $this->pageConfig = $pageConfig;
         parent::__construct($context);
     }
 
     public function execute()
     {
-        return $this->resultPageFactory->create();
+        $resultPage=$this->resultPageFactory->create();
+
+        $resultPage->getConfig()->getTitle()->set(
+            $this->scopeConfig->getValue('faq/general/title', ScopeInterface::SCOPE_STORE)
+        );
+        $resultPage->getConfig()->getTitle()->set(
+            $this->scopeConfig->getValue('faq/general/keywords', ScopeInterface::SCOPE_STORE)
+        );
+        $resultPage->getConfig()->getTitle()->set(
+            $this->scopeConfig->getValue('faq/general/description', ScopeInterface::SCOPE_STORE)
+        );
+
+        return $resultPage;
     }
 }
