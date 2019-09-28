5d46aaa4494d81fd10fe36efd76760f9ec388919
igortis <44641113+igortis@users.noreply.github.com>
2019-09-28 20:32:13 +0300

    Add flush cache

--- FAQ/Controller/Adminhtml/Index/MassStatus.php
+++ FAQ/Controller/Adminhtml/Index/MassStatus.php
@@ -11,6 +11,9 @@ use Magento\Backend\App\Action\Context;
 use Magento\Ui\Component\MassAction\Filter;
 use DevLab\FAQ\Model\ResourceModel\FAQ\CollectionFactory;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Cache\Frontend\Pool;
+
 
 /**
  * Class MassDisable
@@ -27,16 +30,26 @@ class MassStatus extends \Magento\Backend\App\Action
      */
     protected $collectionFactory;
 
+    /** add flush cache */
+    protected $cacheTypeList;
+    protected $cacheFrontendPool;
 
     /**
      * @param Context $context
      * @param Filter $filter
      * @param CollectionFactory $collectionFactory
      */
-    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory,
+                                \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+                                \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
+)
     {
         $this->filter = $filter;
         $this->collectionFactory = $collectionFactory;
+
+        /** add flush cache */
+        $this->_cacheTypeList = $cacheTypeList;
+        $this->_cacheFrontendPool = $cacheFrontendPool;
         parent::__construct($context);
     }
     /**
@@ -54,6 +67,16 @@ class MassStatus extends \Magento\Backend\App\Action
             $item->setStatus($statusValue);
             $item->save();
         }
+
+        /** add flush cache */
+        $types = array('db_ddl');
+        foreach ($types as $type) {
+            $this->_cacheTypeList->cleanType($type);
+        }
+        foreach ($this->_cacheFrontendPool as $cacheFrontend) {
+            $cacheFrontend->getBackend()->clean();
+        }
+
 
         $this->messageManager->addSuccess(__('A total of %1 record(s) have been modified.', $collection->getSize()));
 
