be7380443f69a513f6a6f6489a9958aae4eb4a1f
igortis <44641113+igortis@users.noreply.github.com>
2019-11-12 20:37:27 +0200

    Observer - Events
    Add eventManager for send email to admin from customer

--- FAQ/Controller/Index/Save.php
+++ FAQ/Controller/Index/Save.php
@@ -10,64 +10,45 @@ namespace DevLab\FAQ\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use DevLab\FAQ\Model\FAQFactory;
-use DevLab\FAQ\Model\CustomersEntityFactory;
-use DevLab\FAQ\Model\ResourceModel\CustomersEntity\Collection;
-use DevLab\FAQ\Model\ResourceModel\CustomersEntity\CollectionFactory;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Session\SessionManagerInterface;
-use Magento\Customer\Model\Customer;
-use Magento\Customer\Model\Session;
-use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Manager;
 
 class Save extends Action
 {
 
     protected $_modelFAQFactory;
-    protected $_modelCustomersEntityFactory;
     protected $_resultPageFactory;
     protected $_sessionManager;
-    private $collectionFactory;
+    protected $eventManager;
 
 
     public function __construct(
         Context $context,
         FAQFactory $modelFAQFactory,
-        CustomersEntityFactory $modelCustomersEntityFactory,
         PageFactory  $resultPageFactory,
-        CollectionFactory $collectionFactory,
-        SessionManagerInterface $sessionManager
+        SessionManagerInterface $sessionManager,
+        Manager $eventManager
 
     )
     {
         parent::__construct($context);
         $this->_modelFAQFactory = $modelFAQFactory;
-        $this->_modelCustomersEntityFactory = $modelCustomersEntityFactory;
         $this->_resultPageFactory = $resultPageFactory;
-        $this->collectionFactory = $collectionFactory;
         $this->_sessionManager = $sessionManager;
+        $this->eventManager = $eventManager;
      }
-    public function getCustomersEntity()
-    {
-        return $this->collectionFactory->create()->getItems();
-    }
+
 
     public function execute()
     {
 
 
-
-        /** data from customer_entity  */
-        $collection = $this->getCustomersEntity();
-
-        $resultRedirect     = $this->resultRedirectFactory->create();
         $FAQModel  = $this->_modelFAQFactory->create();
         /** Get data from form*/
-        $data               = $this->getRequest()->getPost();
-        $date               = date('Y-m-d h:i:sa');
+        $data = $this->getRequest()->getPost();
 
-
-
-        /** Start new code */
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $customerSession = $objectManager->create('Magento\Customer\Model\Session');
         if ($customerSession->isLoggedIn() == true) {
@@ -77,65 +58,28 @@ class Save extends Action
             if ( $data['question'] !="") {
                 /** save custom data */
                 $this->messageManager->addSuccess(__('This user can add question because he is registred'));
-                $FAQModel->setData('question', $data['question']);
-                /** status disable */
-                $FAQModel->setData('status', 0);
                 $FAQModel->setData('customer_id', $customerSession->getCustomer()->getId());
+                $FAQModel->setData('question', $data['question']);
+                $FAQModel->setData('status', 0);
+                $FAQModel->save();
+
+                /** Events */
+                $this->eventManager->dispatch('devlab_faq_adding', [
+                    'customer' => (array)$customerSession->getCustomer()->getData(),
+                    'faq' => (array)$FAQModel->getData()
+                ]);
+
+                $this->_redirect('faq/index/index');
             } else {
                 $this->messageManager->addSuccess(__('Field Question is empty. Please add some worlds.'));
             }
 
-            /**
-            $FAQModel->setData('name', $customerSession->getCustomer()->getName());
-            */
-            $FAQModel->save();
-            $this->_redirect('faq/index/index');
         }
         else {
             $this->messageManager->addSuccess(__('This user can`t add question because he is not registred'));
             $this->_redirect('faq/index/index');
         }
 
-/**
-        $customer_check = $objectManager->get('Magento\Customer\Model\Customer');
-        $customer_check->setWebsiteId($website_id);
-        $customer_check->load('CUSTOMER_ID');
-
-        if ( $customer_check->getId() ) {
-            // the customer already exist
-            $this->messageManager->addSuccess(__('Your question has been saved Not'));
-        } else {
-            // does not exist
-            $this->messageManager->addSuccess(__('This user is NOT registred Ok'));
-        }
-
-*/
-        /**
-        foreach ($collection as $item):
-            if ($data['email'] == $item->getEmail() ){
-
-                $FAQModel->setData('name', $data['name']);
-                $FAQModel->setData('email', $data['email']);
-                $FAQModel->setData('question', $data['question']);
-                $FAQModel->setData('creation_time', $date);
-
-                $FAQModel->save();
-
-                $this->_redirect('faq/index/index');
-                $this->messageManager->addSuccess(__('Your question has been saved'));
-                $my_count_foreach = 1;
-            }
-
-        endforeach;
-        */
-        /** if user is not registered */
-        /**
-        if ( $my_count_foreach == 0  ){
-            $this->_redirect('faq/index/index');
-            $this->messageManager->addSuccess(__('This user is NOT registred'));
-        }
-        */
-        /** ($data['email'] != $item->getEmail() */
 
     }
 }
